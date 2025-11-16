858c81faafdd95794ca28951cf7b57880c12689b
SAMKIEL <samuelezekiel488@gmail.com>
2025-11-16 06:28:38 +0100

    `feat: updated package.json and added react-router-dom to frontend`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
+
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import './index.css'
@@ -6,9 +8,16 @@ import './index.css'
 
 const App = () => {
   return (
-      <div> 
+      <div>
+
        <Navbar />
-       <Home />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/same" element={<Home />} />
+          <Route path="*" element={<h2>404 - Not found</h2>} />
+        </Routes>
+
       </div>
   )
 }
