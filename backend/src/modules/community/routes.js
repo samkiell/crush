f2fdd030c7571a1be0f25b02eda838ffeba02138
SAMKIEL <samuelezekiel488@gmail.com>
2025-11-23 18:32:27 +0100

    feat: Add `markmap.svg` image asset.

--- backend/src/modules/community/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder routes - to be implemented
-router.get('/', (req, res) => {
-  res.json({ message: 'Community routes - Coming soon' });
-});
-
-module.exports = router;
